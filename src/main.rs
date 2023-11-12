4ed4615789faf69a03bd67e6947869f6239d684b
Lukas Nöllemeyer <babelfisch92@gmx.de>
2023-11-12 21:57:06 +0100

    removed old commented code

--- src/main.rs
+++ src/main.rs
@@ -54,24 +54,4 @@ pub fn main() {
         }
         thread::sleep(Duration::from_millis(500));
     }
-    // emu.set_fen("startpos");
-    // emu.set_difficulty(Some(4)).unwrap();
-    // //emu.force_moves(vec![chess::ChessMove::from_str("d2d4").unwrap()]);
-    // let input = stdin();
-    // let mut rl = String::new();
-    // loop {
-    //     println!("CPU move: {}", emu.gen_move().unwrap());
-    //     println!("{}\nEnter Move", emu.cur_board);
-    //     rl.clear();
-    //     match input.read_line(&mut rl) {
-    //         Ok(_) => {
-    //             let in_str = rl.trim();
-    //             emu.play_move(chess::ChessMove::from_str(in_str).unwrap());
-    //             println!("{}", emu.cur_board);
-    //         }
-    //         Err(e) => {
-    //             panic!("Error reading from stdin: {}", e)
-    //         }
-    //     }
-    // }
 }
